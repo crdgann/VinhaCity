1982493a5e06d9659e6a82453c654a5e40e0aed1
ArthurAbreu <78042477+ArthurAbreu2005@users.noreply.github.com>
2023-04-27 21:28:36 -0300

    script que contem as questoes do QUIZ

---
+++ questions.js
@@ -0,0 +1,72 @@
+export default [
+    {
+      question: "O vinho branco é feito apenas de uvas brancas?",
+      answers: [
+        { option: "V", correct: false },
+        { option: "F", correct: true },
+      ],  
+    },
+    {
+      question: "O vinho tinto deve ser servido em temperatura ambiente?",
+      answers: [
+        { option: "V", correct: false },
+        { option: "F", correct: true },
+      ],
+    },
+    {
+      question: "O champanhe é sempre feito na região de Champagne, na França?",
+      answers: [
+        { option: "V", correct: true },
+        { option: "F", correct: false },
+      ],
+    },
+    {
+      question: "O sabor do vinho é influenciado pelo terroir, que é o ambiente em que as uvas crescem?",
+      answers: [
+        { option: "V", correct: true },
+        { option: "F", correct: false},
+      ],
+    },
+    {
+        question: "O vinho rosé é uma mistura de vinho branco e vinho tinto?",
+        answers: [
+            { option: "V", correct: false },
+            { option: "F", correct: true },
+        ],
+      },
+      {
+        question: "Vinhos orgânicos são sempre melhores do que vinhos não orgânicos?",
+        answers: [
+            { option: "V", correct: false },
+            { option: "F", correct: true },
+        ],
+      },
+      {
+        question: "O teor alcoólico do vinho é sempre indicado no rótulo?",
+        answers: [
+            { option: "V", correct: false },
+            { option: "F", correct: true },
+        ],
+      },
+      {
+        question: "O vinho de mesa é sempre de baixa qualidade?",
+        answers: [
+            { option: "V", correct: false },
+            { option: "F", correct: true },
+        ],
+      },
+      {
+        question: "O enólogo é o profissional responsável por fazer o vinho?",
+        answers: [
+          { option: "V", correct: true },
+          { option: "F", correct: false },
+        ],
+      },
+      {
+        question: "O decanter é usado para remover sedimentos do vinho?",
+        answers: [
+          { option: "V", correct: true },
+          { option: "F", correct: false },
+        ],
+      },
+  ];
